8cab4dedb80dcd956946e8c0550f01a502ac873a
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-12-26 17:43:46 -0500

    example with button

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Progress, Button } from 'reactstrap'
 
 import useAutoProgress from 'use-auto-progress'
 
 const App = () => {
-  const [value] = useAutoProgress(true);
+  const [running, setRunning] = useState(false);
+  const [value, setStart] = useAutoProgress(running);
+
+  const toggle = () => {
+    const s = !running;
+    setRunning(s);
+    setStart(s);
+  };
+
   return (
     <div>
-      {value}
+      <Button onClick={toggle}>{running ? 'Stop' : 'Start'}</Button>
+      <Progress value={value}>{value}%</Progress>
     </div>
   )
 }
