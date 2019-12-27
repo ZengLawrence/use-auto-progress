f411862d1444f77e862f6f04b32a0791c4fef1d1
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-12-27 08:28:06 -0500

    add steps option

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 
-const PROGRESS_VALUES = [0, 25, 75, 90];
-const INITIAL_VALUE = PROGRESS_VALUES[0];
+const DEFAULT_STEPS = [0, 25, 75, 90];
+const INITIAL_VALUE = 0;
 const FINAL_VALUE = 100;
 
-const useValueState = () => {
+const useValueState = (steps: number[]) => {
     const [value, setValue] = useState(INITIAL_VALUE);
 
     const finalValue = () => (setValue(FINAL_VALUE));
@@ -14,12 +14,13 @@ const useValueState = () => {
             setValue(val);
         }, ms);
     }
-    const scheduleTimers = (ms: number) => PROGRESS_VALUES.map((v, i) => setValueTimer(v, ms * i));
+    const scheduleTimers = (ms: number) => steps.map((v, i) => setValueTimer(v, ms * i));
     return { value, finalValue, scheduleTimers };
 }
 
-const useAutoProgress = (start: boolean, options: { intervalMs: number } = { intervalMs: 500 }): [number, (start: boolean) => void, boolean] => {
-    const { value, finalValue, scheduleTimers } = useValueState();
+const useAutoProgress = (start: boolean, options: { intervalMs?: number, steps?: number[] } = {}): [number, (start: boolean) => void, boolean] => {
+    const { intervalMs, steps } = { intervalMs: 500, steps: DEFAULT_STEPS, ...options };
+    const { value, finalValue, scheduleTimers } = useValueState(steps);
     const [running, setRunning] = useState(start);
 
     const [timers, setTimers] = useState<NodeJS.Timeout[]>([]);
@@ -29,7 +30,7 @@ const useAutoProgress = (start: boolean, options: { intervalMs: number } = { int
         if (s !== running) {
             setRunning(s);
             if (s) {
-                setTimers(scheduleTimers(options.intervalMs));
+                setTimers(scheduleTimers(intervalMs));
             } else {
                 cancelTimers();
                 finalValue();
