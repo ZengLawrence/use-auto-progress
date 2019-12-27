f472085edae510b443f46d542133c37135629338
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-12-27 07:23:01 -0500

    set timer by useValueState

--- src/index.tsx
+++ src/index.tsx
@@ -8,25 +8,26 @@ const useValueState = () => {
     const [value, setValue] = useState(INITIAL_VALUE);
 
     const finalValue = () => (setValue(FINAL_VALUE));
-    return { value, setValue, finalValue };
-}
-
-const useAutoProgress = (start: boolean): [number, (start: boolean) => void] => {
-    const { value, setValue, finalValue } = useValueState();
-    const [startProgress, setStartProgress] = useState(start);
-    const [timers, setTimers] = useState<NodeJS.Timeout[]>([]);
 
     const setValueTimer = (val: number, ms: number) => {
         return setTimeout(() => {
             setValue(val);
         }, ms);
     }
+    const scheduleTimers = (ms: number) => PROGRESS_VALUES.map((v, i) => setValueTimer(v, ms * i));
+    return { value, finalValue, scheduleTimers };
+}
+
+const useAutoProgress = (start: boolean): [number, (start: boolean) => void] => {
+    const { value, finalValue, scheduleTimers } = useValueState();
+    const [startProgress, setStartProgress] = useState(start);
+    const [timers, setTimers] = useState<NodeJS.Timeout[]>([]);
+
     const setStart = (s: boolean) => {
         if (s !== startProgress) {
             setStartProgress(s);
             if (s) {
-                const setValueTimers = PROGRESS_VALUES.map((v, i) => setValueTimer(v, 500 * i));
-                setTimers(setValueTimers);
+                setTimers(scheduleTimers(500));
             } else {
                 finalValue();
             }
